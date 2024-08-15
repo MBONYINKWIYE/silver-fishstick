4407027be864794a7a15e3b4d53708ed825759a1
Frank <148916030+MBONYINKWIYE@users.noreply.github.com>
2024-08-15 23:54:50 +0200

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,6 @@
+#The introductory question with the answers will be printed out
+
+name = str(input("Enter your Full names please: "))    #to store the names of user
+age = int(input("Enter your age :"))    #stores age integer numbers
+location = str(input("where are you locating? :"))    #stores location of user in string 
+print(f"Hello {name} , you are {age} years old and live in {location}")
